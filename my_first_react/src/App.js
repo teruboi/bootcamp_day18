b8ac94d9a889665835872a7c5d1073acdb9d3b44
teruboi <reynaldinickolas@gmail.com>
2023-02-21 12:06:24 +0700

    Added form componen to search images by query from unsplash.com API

--- my_first_react/src/App.js
+++ my_first_react/src/App.js
@@ -1,6 +1,10 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import moment from "moment";
 import './App.css';
+//importing unsplash API to be used in here
+import unsplash from './unsplash';
+
+
 
 //Module for navbar, copied from Bootstrap and modified
 function Navbar() {
@@ -31,145 +35,55 @@ function Navbar() {
   );
 }
 
-//Module for the body, in this case only heading text
-function Body() {
-  return (
-    <div class="container-fluid">
-      <h1>This is React</h1>
-    </div>
-  );
-}
+//Component for the main app
+class App extends React.Component {
+  //Where the image data will be stored
+  //from unsplash.com
+  state = { images: [] }
+  
+  //function to find image associated with the query
+  //on unsplash.com, then set the data to state.images
+  //above.
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term }
+    })
 
-//Component for the main chat / comment app
-function App(props) {
-
-  //Calling the data from props
-  const data = props.data
-  // console.log(data);
-
-  //Looping return for each data in the array
-  const comments = data.map((e) => {
-
-    //Configuring past day until present
-    const date = moment().format()
-    const diff = moment(date).diff(e.time, 'days')
-
-    let day
-
-    //If the day difference between today and comment date is 0
-    //prints out "Today"
-    //else prints out "... days ago"
-    if (diff === 0) {
-      day = 'Today'
-    } else {
-      day = moment(date).to(e.time)
-    }
-
-    //Returning the whole comment component
-    return(
-      <div class="comment">
-        <a class="avatar">
-          <img alt='avatar' src={e.avatar} />
-        </a>
-        <div class="content">
-          <a class="author" >{e.name}</a>
-          <div class="metadata">
-            <span class="date">{day} at {moment(e.time).format('LT')}</span>
-          </div>
-          <div class="text">
-            {e.comment}
-          </div>
-        </div>
-      </div>
-    )  
-  })
-
-  //HTML (JSX)
-  return( 
-    <div class="ui comments">
-      {comments}
-    </div>
-  )
-}
-
-//Class for the comment container,
-//converted from the function above
-class CommentContainer extends React.Component {
-  //Props used in the class
-  //In this case, the comment data
-  //(Name, Likes, Date, Comment, PP)
-  constructor (props) {
-    super(props)
-    this.state = {
-      count: parseInt(this.props.likes)
-    }
+    this.setState({ images: response.data.results })
+    console.log(this.state.images);
   }
+
+  //rendering the HTML body
   render() {
     return (
-      <div className='ui container comments'>
-        <div className='comment'>
-          <a href='/' className='avatar'>
-            <img alt='avatar' src={this.props.avatar} />
-          </a>
-          <div className='content'>
-            <a href='/' className='author' style={{textDecoration: "none"}}>
-              {this.props.name}
-              <div className='metadata'>
-                <span className='date'>
-                  {this.props.day} at {this.props.time}
-                </span>
-              </div>
-              {/* Likes count */}
-              | Likes: {this.state.count}
-            </a>
-            <div className='text'>
-              {this.props.comment}
-            </div>
-            {/* Button for adding 1 like to Likes */}
-            <div className='mini ui icon red button' onClick={()=>this.setState({ count: this.state.count + 1 })}>
-              <i className='heart icon'></i> Like
-            </div>
-          </div>
+      <div className='ui fluid container centered'>
+        <div className='ui container' style={{ marginTop: '10px' }}>
+          {/* Component searchBar for query input, that will be used for search the images */}
+          <SearchBar onSubmit={this.onSearchSubmit} />
+        </div>
+        <div className='ui grid centered container' style={{ marginTop: '10px', alignContent: 'center'}}>
+          {/* Component for rendering the images called from API */}
+          <Images data={this.state.images} />
         </div>
       </div>
+    
     )
   }
 }
 
-//Calling the CommentContainer component with
-//props from database
-class Comments extends React.Component {
-  render() {
-    return this.props.data.map((dataComment, index) => (
-      <div className='CommentContainer' key={index}>
-        <CommentContainer
-          avatar={dataComment.avatar}
-          name={dataComment.name}
-          day={moment(dataComment.time).format("ddd")}
-          time={moment(dataComment.time).format("LT")}
-          comment={dataComment.comment}
-          likes={dataComment.like}
-        />
-      </div>
-    ))
-  }
-}
-
-//Example of data counting dynamically
-class Counting extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      count: 0
-    }
-  }
+//Images renderer
+class Images extends React.Component {
   render() {
     return (
-      <div>
-        <h1>you clicked {this.state.count} times</h1>
-        <button onClick={()=>this.setState({ count: this.state.count + 1 })}>click on me!</button>
-      </div>
-    )
+      //Using map function to output all image in the data array
+       this.props.data.map((data, index) => (
+        // using semantic ui and inline css
+        // to group the image into 4 images/row with same ratio
+        <div className='four wide column' key={index}>
+          <img className='ui image Images' src={data.urls.small_s3} alt={data.alt_description} style={{ objectFit: 'cover', width: 300, height: 300 }}/>
+        </div>
+       ))
+    );
   }
 }
 
@@ -209,5 +123,74 @@ class Clock extends React.Component {
     )
   }
 }
+
+//Component for handling form input
+//In this case for submitting name
+class Form extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: "" }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value })
+  }
+
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.value)
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
+}
+
+//Component to add search bar to body that
+// will call the function in props
+class SearchBar extends React.Component{
+  state = { term: "" }
+
+  //The function calls for another function
+  //called in the component props
+  onFormSubmit = (event) => {
+    event.preventDefault()
+
+    this.props.onSubmit(this.state.term)
+  }
+
+  //Rendering the search bar body to HTML
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className='field'>
+            <label>Image Search</label>
+            <input
+              type='text'
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
 //Exporting components to index.js
-export { Navbar, Body, App, CommentContainer, Comments, Counting, Clock };
+export { Navbar, Body, App, CommentContainer, Comments, Counting, Clock, Form, SearchBar };
--- my_first_react/src/index.js
+++ my_first_react/src/index.js
@@ -4,56 +4,20 @@ import ReactDOM from 'react-dom';
 import { faker } from "@faker-js/faker";
 import moment from 'moment';
 
+
 //Importing components from App.js
-import { Navbar, Body, Comments, Counting, Clock } from './App';
+import { App, Navbar, Body, Comments, Counting, Clock, Form, SearchBar } from './App';
 import './index.css';
+
+
 
 //Function to simplify rendering to HTML
 function render(content, id) {
   ReactDOM.render(content, document.getElementById(id))
 }
 
-//Data faker
-const data = [
-  {
-    name: faker.name.firstName(),
-    avatar: faker.image.avatar(),
-    time: faker.date.recent(3),
-    comment: faker.lorem.lines(1),
-    like: faker.random.numeric()
-  },
-  {
-    name: faker.name.firstName(),
-    avatar: faker.image.avatar(),
-    time: faker.date.recent(3),
-    comment: faker.lorem.lines(1),
-    like: faker.random.numeric()
-  },
-  {
-    name: faker.name.firstName(),
-    avatar: faker.image.avatar(),
-    time: faker.date.recent(3),
-    comment: faker.lorem.lines(1),
-    like: faker.random.numeric()
-  }
-]
-
-//Rendering to root using createRoot
-const el = document.getElementById("root")
-const root = ReactDOM.createRoot(el)
-
-//Calling Comments component to App
-const App = () => {
-  return (
-    <div>
-      {<Comments data={data} />}
-    </div>
-  )
-}
-
 //Rendering App component
-render(<Clock />, "clock")
-render(<App data={data} />, "root")
+render(<App />, "root")
 //Renderer
 // render(<Navbar />, "navbar")
 // render(<Body />, "root")
